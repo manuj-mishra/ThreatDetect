e3b0cb23966580a0c435836080bda8b4d8f40866
douglasadams201199 <douglas.adams99@hotmail.com>
2024-06-01 22:04:56 +0100

    Adding LLM

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,20 @@ import base64
 import json
 from flask_cors import CORS
 from PIL import Image, ImageDraw
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+from io import BytesIO
+import re
+
+# Load environment variables from a .env file
+load_dotenv()
+
+openai_api_key = os.environ.get('OPENAI_API_KEY')
+os.environ['OPENAI_API_KEY'] = openai_api_key
+
+client = OpenAI()
+
 
 app = Flask(__name__)
 CORS(app)
@@ -40,11 +54,48 @@ def place_object():
         
     return img_string
 
-def dummy_llm_call(image_string, map_string, prompt):
-    # This is a dummy function, so we're not actually using the inputs.
-    # In a real implementation, you would use these inputs to generate some output.
-    return "default output"
+system_prompt = """
+You are a helpful assistant. You are designed to look at an image and locate on a provided map the grid reference that that object is in. 
+You should only return this string, for example "B2" and nothing more. Do not add any fluff or explanation to your answer. 
+"""
+
+def dummy_llm_call(image_string, map_string, system_prompt):
+
+    user_prompt = f"""
+        The object you are looking for is: A white cap.
+        Remember, only return one classification with the letter first then the number
+        """
+
+    response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": [
+                    {"type": "text", "text": user_prompt},
+                    {"type": "image_url", "image_url": {
+                        "url": f"data:image/png;base64,{image_string}"}
+                    },
+                    {"type": "image_url", "image_url": {
+                        "url": f"data:image/png;base64,{map_string}"} 
+                    }
+                ]}
+            ]
+        )
+    
+    text = response.choices[0].message.content
+    match = re.search(r'[A-Z]\d', text)
+    if match:
+        return print(match.group(0))
+    else:
+        return "No match found"
+
 
 if __name__ == '__main__':
     app.run()
     #place_object()
+    # app.run()
+    # place_object()
+    # dummy_llm_call(image_string, map_string, system_prompt)
+
+
+
